8fdbb9cb44247e51c6cf509c3ea7315e78d0c386
Idris Y <sld7700@gmail.com>
2017-06-16 10:19:28 +0300

    Better classes. The waiting functionality needs to be done

--- main.py
+++ main.py
@@ -4,31 +4,26 @@ from transitions import Machine
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-text =  ("The Notre Dame football team has a long history, first beginning"
-         "when the Michigan Wolverines football team brought football to Notre Dame in"
-         "1887 and played against a group of students. In the long history since then,"
-         "13 Fighting Irish teams have won consensus national championships (although"
-         "the university only claims 11), along with another nine teams being named"
-         "national champion by at least one source. Additionally, the program has the"
-         "most members in the College Football Hall of Fame, is tied with Ohio State"
-         "University with the most Heisman Trophies won, and have the highest winning"
-         "percentage in NCAA history. With the long history, Notre Dame has"
-         "accumulated many rivals, and its annual game against USC for the Jeweled"
-         "Shillelagh has been named by some as one of the most important in college"
-         "football and is often called the greatest intersectional rivalry in college"
-         "football in the country.")
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-class Bot:
-    states = ['start', 'ask', 'long']
+class BotFSM:
+    states = ['init', 'start', 'ask', 'long']
 
     def __init__(self):
-        self.machine = Machine(model=self, state=Bot.states, initial='start')
+        self.machine = Machine(model=self, states=BotFSM.states, initial='init')
 
+        self.machine.add_transition('start', 'init', 'start')
         self.machine.add_transition('ask_question', 'start', 'ask')
         self.machine.add_transition('long_waiting', 'ask', 'long')
         self.machine.add_transition('too_long_waiting', 'long', 'long')
 
+    def start(self):
+        print("Begining the journey!")
 
     def ask_question(self):
         print("How are you?")
@@ -40,43 +35,79 @@ class Bot:
         print("Why so silence?")
 
 
-def start_cmd(bot, update):
-    message = 'Hello Mighty {}!'.format(update.effective_user.first_name)
-    update.message.reply_text(message)
+class Bot:
+    text =  ("The Notre Dame football team has a long history, first beginning"
+         " when the Michigan Wolverines football team brought football to Notre Dame in"
+         " 1887 and played against a group of students. In the long history since then,"
+         " 13 Fighting Irish teams have won consensus national championships (although"
+         " the university only claims 11), along with another nine teams being named"
+         " national champion by at least one source. Additionally, the program has the"
+         " most members in the College Football Hall of Fame, is tied with Ohio State"
+         " University with the most Heisman Trophies won, and have the highest winning"
+         " percentage in NCAA history. With the long history, Notre Dame has"
+         " accumulated many rivals, and its annual game against USC for the Jeweled"
+         " Shillelagh has been named by some as one of the most important in college"
+         " football and is often called the greatest intersectional rivalry in college"
+         " football in the country.")
 
-    message = ("I'm Convai.io bot #1337. My main goal is to talk about the text"
-               " I provided below. You can ask me questions about the text or I can do the same."
-               " Type /help to get some more information.")
-    update.message.reply_text(message)
+    def __init__(self):
+        self._fsm = BotFSM()
+        token = "381793449:AAEogsUmzwqgBQiIz6OmdzWOY6iU_GwATeI"
+        self._updater = Updater(token)
 
-    update.message.reply_text("The text: \"{}\"".format(text))
-    update.message.reply_text("Also you can the get text by typing /text command")
+        dp = self._updater.dispatcher
+        dp.add_handler(CommandHandler("start", self._start_cmd))
+        dp.add_handler(CommandHandler("help", self._help_cmd))
+        dp.add_handler(CommandHandler("text", self._text_cmd))
 
+        dp.add_handler(MessageHandler(Filters.text, self._echo_cmd))
+        dp.add_error_handler(self._error)
 
-def help_cmd(bot, update):
-    message = """\start - shows greeting message
-\\text - shows the text
-\help - shows this message."""
-    update.message.reply_text(message)
+    def start(self):
+        self._updater.start_polling()
+        self._updater.idle()
 
+    def _start_cmd(self, bot, update):
+        message = 'Hello Mighty {}!'.format(update.effective_user.first_name)
+        update.message.reply_text(message)
 
-def text_cmd(bot, update):
-    update.message.reply_text("The text: \"{}\"".format(text))
+        message = ("I'm Convai.io bot #1337. My main goal is to talk about the text"
+                   " I provided below. You can ask me questions about the text or I can do the same."
+                   " Type /help to get some more information.")
+        update.message.reply_text(message)
 
+        update.message.reply_text("The text: \"{}\"".format(Bot.text))
+        update.message.reply_text("Also you can the get text by typing /text command")
 
-def main():
-    token = "381793449:AAEogsUmzwqgBQiIz6OmdzWOY6iU_GwATeI"
-    updater = Updater(token)
+        self._fsm.start()
 
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start_cmd))
-    dp.add_handler(CommandHandler("help", help_cmd))
-    dp.add_handler(CommandHandler("text", text_cmd))
+    def _help_cmd(self, bot, update):
+        message = ("\start - shows greeting message\n"
+                   "\\text - shows the text\n"
+                   "\help - shows this message.")
+        update.message.reply_text(message)
 
-    updater.start_polling()
-    updater.idle()
+    def _text_cmd(self, bot, update):
+        update.message.reply_text("The text: \"{}\"".format(Bot.text))
+
+    def _echo_cmd(self, bot, update):
+        self._save_chat_info(update)
+
+        if self._fsm.state == 'init':
+            update.message.reply_text(
+                "{}, please type /start to begin the journey.".format(self._user_first_name)
+            )
+        else:
+            update.message.reply_text("You type me: {}".format(update.message.text))
+
+    def _save_chat_info(self, update):
+        self._user_first_name = update.effective_user.first_name
+
+    def _error(self, bot, update, error):
+        logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 
 if __name__ == '__main__':
-    main()
+    bot = Bot()
+    bot.start()
 
