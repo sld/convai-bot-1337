3e35aac28c036bade84bf61063f4ab9286318ed1
Idris Y <sld@users.noreply.github.com>
2018-01-03 12:29:20 +0500

    Add opennmt_chitchat/data/ in setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ cp data/model.t7 question_generation/data/
 cp data/textsum_epoch7_14.69_release.t7 opennmt_summarization/models/
 cp data/fbchichat_ver2_epoch9.t7 fbnews_chitchat/data/
 cp data/glove.6B.100d.txt intent_classifier/data/
+cp data/CPU_epoch5_14.62.t7 opennmt_chitchat/data/
 
 # additional data for question answerer
 wget https://www.dropbox.com/s/rx7heizq5y8wi2j/factoid_question_answerer_data.tar.gz
